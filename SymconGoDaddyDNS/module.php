b6dd9a3ba4a767d46c25fbacf4f464ab04e50def
elueckel <30531515+elueckel@users.noreply.github.com>
2019-01-20 17:58:57 +0100

    Update module.php

--- SymconGoDaddyDNS/module.php
+++ SymconGoDaddyDNS/module.php
@@ -66,7 +66,7 @@ if (!defined('vtBoolean')) {
 			$Key = $this->ReadPropertyString("APIKey");
 			$Secret = $this->ReadPropertyString("Secret");
 			$IPInfoToken = $this->ReadPropertyString("IPInfoToken");
-			$DNSUpdate = $this->ReadPropertyBoolean("DNSUpdate");
+			$DNSUpdate = $this->ReadPropertyString("DNSUpdate");
 			$PublicIPVariable = $this->ReadPropertyBoolean("PublicIPVariable");
 			$Debug = $this->ReadPropertyBoolean("Debug");	
 			
